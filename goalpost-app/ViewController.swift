eac11a7ebaeb810471aa3273bee72e24633bf521
Sergey Neupokoev <Sergey@Sergeys-MacBook-Pro.local>
2020-01-19 23:15:53 +0400

    Initiating the project

---
+++ goalpost-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by Sergey Neupokoev on 19/1/20.
+//  Copyright © 2020 Sergey Neupokoev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
