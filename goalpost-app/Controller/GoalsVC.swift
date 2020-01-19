9309ea2b4db146ecf49cccf4de36dcad29d4886a
Sergey Neupokoev <Sergey@Sergeys-MacBook-Pro.local>
2020-01-19 23:44:49 +0400

    Added new files and built the interface

---
+++ goalpost-app/Controller/GoalsVC.swift
@@ -0,0 +1,26 @@
+//
+//  GoalsVC.swift
+//  goalpost-app
+//
+//  Created by Sergey Neupokoev on 19/1/20.
+//  Copyright © 2020 Sergey Neupokoev. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func addGoalButtonWasPressed(_ sender: UIButton) {
+        print("button was pressed")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
