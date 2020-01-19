a81d5dcf4c25c0a4c90242e1b625cfec68e5c85c
Sergey Neupokoev <Sergey@Sergeys-MacBook-Pro.local>
2020-01-20 00:06:47 +0400

    Interface builder job done

---
+++ goalpost-app/View/GoalCell.swift
@@ -0,0 +1,23 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by Sergey Neupokoev on 19/1/20.
+//  Copyright © 2020 Sergey Neupokoev. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescriptionLabel: UILabel!
+    @IBOutlet weak var goalTypeCell: UILabel!
+    @IBOutlet weak var goalProgressLabel: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int){
+        self.goalDescriptionLabel.text = description
+        self.goalTypeCell.text = type
+        self.goalProgressLabel.text = String(describing: goalProgressAmount)
+    }
+    
+}
